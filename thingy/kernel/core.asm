20720ca36f10611224c8adb686a9b23a45385a83
theverygaming <theverygaming@users.noreply.github.com>
2026-02-13 10:36:19 +0100

    thingy: read extra sectors at entry

--- thingy/kernel/core.asm
+++ thingy/kernel/core.asm
@@ -11,14 +11,62 @@ rjmp _entry
 
 .section .text
 _entry:
-shr r1, r1, #2 // r1 / 4
-add rsp, r0, r1 // rsp = top of memory
+add rsp, r0, r1
+sub rsp, rsp, #4 // rsp = top of memory
+// save r0 and r1 on the stack
+push(r0)
+push(r1)
 
-mov r0, #0b1 // set alignment flag
-mtsr pcst, r0
-mov r0, #0 // int_handler
-or r0, r0, #0x1 // no jump table
-mtsr ibptr, r0
+mov r2, #0b1 // set alignment flag
+mtsr pcst, r2
+
+// read as many sectors of HDD as we can! only the first sector isn't enough
+// r11 = max. available sectors
+mov r11, #0xB008
+movh r11, #0xF004
+ldr r11, r11, #0
+mov r12, #0x03
+shl r12, r12, #29
+not r12, r12
+and r11, r11, r12
+
+// r10 = current sector
+mov r10, #1
+
+// r12 = current memory address
+add r12, r0, #512
+
+// r13 = max memory address
+add r13, r0, r1
+// leave 1KiB of space for stack and stuff
+sub r13, r13, #1024
+
+.hdd_read_sectors:
+// have we reached the end of the drive?
+cmp r10, r11
+ifgteq rjmp .hdd_read_sectors_end
+// have we reached the end of memory?
+cmp r12, r13
+ifgteq rjmp .hdd_read_sectors_end
+// debug print
+mov r0, #1
+mov r1, #82 // 'R'
+int #128
+// read the sector
+mov r0, #0 // function = read sector from disk
+mov r1, r10 // sector number
+mov r2, r12 // memory address
+int #128
+// increment sector & memory counters
+add r10, r10, #1
+add r12, r12, #512
+rjmp .hdd_read_sectors
+.hdd_read_sectors_end:
+
+// debug print
+mov r0, #1
+mov r1, #69 // 'E'
+int #128
 
 rjal kmain
 // jump to ROM start, hopefully resetting the machine
